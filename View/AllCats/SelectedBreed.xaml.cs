4e9f406fd8cf9ed865640e7ae826cde0d1e01647
Dmitry Belogorsky <dbelogorskiy@gmail.com>
2024-12-22 13:19:24 +0300

    Update 22/12/2024 Add Share Button (breed)

--- View/AllCats/SelectedBreed.xaml.cs
+++ View/AllCats/SelectedBreed.xaml.cs
@@ -12,4 +12,21 @@ public partial class SelectedBreed : ContentPage
     {
         await Navigation.PopAsync(); // Переход на предыдущую страницу
     }
+
+    private async void ShareBtn_Clicked(object sender, EventArgs e)
+    {
+        string textToShare = $"Порода: {BreedTB.Text}\nОписание: {DescriptionTB.Text}";
+        try
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = textToShare,
+                Title = "Поделиться текстом"
+            });
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ошибка", $"Не удалось поделиться текстом: {ex.Message}", "OK");
+        }
+    }
 }
